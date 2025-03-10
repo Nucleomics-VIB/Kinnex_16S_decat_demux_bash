258ec1593c6cd306e224d7dc011d28b49953ce31
Stephane Plaisance <stephane.plaisance@vib.be>
2025-03-10 09:05:23 +0100

    edits
    revio version

--- Kinnex_16S_decat_demux.sh
+++ Kinnex_16S_decat_demux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # script: Kinnex_16S_decat_demux.sh
-# run skera and lima on a Kinnex 16S RUN
+# run skera and lima on a Revio multiplexed Kinnex 16S RUN
 #
 # Stephane Plaisance - VIB-NC 2024-06-03 v1.0
 # small edits in the header below: v1.0.1
@@ -13,65 +13,72 @@
 # conda env: Kinnex_16S_decat_demux_env (installed from conda_env_setup.yaml)
 # => conda env create -f conda_env_setup.yaml
 # config.yaml (edited and pointing to existing files)
-# yq (to read the yaml config file into bash variables)
+# yq (version 4+ to read the yaml config file into bash variables)
 # PacBio barcode files copied to barcode_files)
-# pigz for archiving speedup
-# a BAM file resulting from a Kinnez 16S run
+# a BAM file resulting from a Kinnex 16S run
 # a samplesheet linking barcode pairs to sample names (custom made)
 # All parameters have been externalised from the code and are listed in config.yaml
 
-version="2024-06-14; 1.1.0"
+version="2025-03-07; 1.2.0"
+
+# script basedir
+BASEDIR=$(dirname "$(readlink -f "$0")")
 
 myenv="Kinnex_16S_decat_demux_env"
-source /etc/profile.d/conda.sh
-conda activate ${myenv} || \
-  ( echo "# the conda environment ${myenv} was not found on this machine" ;
-    echo "# please read the top part of the script!" \
-    && exit 1 )
 
-# check executables present
-declare -a arr=( "yq" "skera" "lima" "bam2fastq" "pigz" "scripts/barcode_QC_Kinnex.sh" )
-for prog in "${arr[@]}"; do
-$( hash ${prog} 2>/dev/null ) || ( echo "# required ${prog} not found in PATH"; exit 1 )
-done
-
-# Check if config.yaml exists
-if [ ! -f "config.yaml" ]; then
-    echo "config.yaml file not found. Please create the config.yaml file."
-    exit 1
-fi
-
-# Load variables from config.yaml
-while IFS= read -r line; do
-    # Split the line into key and value
-    key=$(echo "$line" | cut -d':' -f1)
-    value=$(echo "$line" | cut -d':' -f2-)
-
-    # Remove any leading or trailing whitespace from the key and value
-    key=${key##* }
-    key=${key% *}
-    value=${value##* }
-    value=${value% *}
-
-    # Remove double-quotes from the value
-    value=${value//\"/}
-
-    # Assign the value to the Bash variable
-    declare "$key"="$value"
-done <  config.yaml
-
-# conda does not install yq version 4 required for the following command syntax
-# eval $(yq e '. as $item ireduce ("";  . + "export " + ($item | to_entries | .[] | .key + "=\"" + .value + "\" "))' config.yaml)
-
-mkdir -p ${outfolder}
-
-# redirect all outputs to a log file
-cat /dev/null > ${outfolder}/runlog.txt
-exec &> >(tee -a ${outfolder}/runlog.txt)
-
+################################
 ########## FUNCTIONS ###########
+################################
 
-function CopyRunData {
+function parse_yaml() {
+    local prefix=$2
+    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+    sed -ne "s|^\($s\):|\1|" \
+         -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+         -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" $1 |
+    awk -F$fs '{
+        indent = length($1)/2;
+        vname[indent] = $2;
+        for (i in vname) {if (i > indent) {delete vname[i]}}
+        if (length($3) > 0) {
+            vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+            printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+        }
+    }'
+}
+
+function PrintConfig() {
+echo "####################"
+echo "# run configuration"
+echo "#"
+echo "- runfolder: $runfolder"
+echo "- movie: $movie"
+echo "- hififolder: $hififolder"
+echo "bcM0001 BAM: $bcM0001_bam"
+echo "bcM0001 Samplesheet: $bcM0001_samplesheet"
+echo "bcM0002 BAM: $bcM0002_bam"
+echo "bcM0002 Samplesheet: $bcM0002_samplesheet"
+echo "bcM0003 BAM: $bcM0003_bam"
+echo "bcM0003 Samplesheet: $bcM0003_samplesheet"
+echo "bcM0004 BAM: $bcM0004_bam"
+echo "bcM0004 Samplesheet: $bcM0004_samplesheet"
+echo "- outfolder: $outfolder"
+echo "- inputs: $inputs"
+echo "- skera_results: $skera_results"
+echo "- lima_results: $lima_results"
+echo "- fastq_results: $fastq_results"
+echo "- log_skera: $log_skera"
+echo "- log_lima: $log_lima"
+echo "- nthr_skera: $nthr_skera"
+echo "- nthr_lima: $nthr_lima"
+echo "- par_bam2fastq: $par_bam2fastq"
+echo "- nthr_bam2fastq: $nthr_bam2fastq"
+echo "- mincnt: $mincnt"
+echo "- qc_format: $qc_format"
+echo "- final_results: $final_results"
+}
+
+function CopyRunData() {
 local flag_file="${outfolder}/${inputs}/CopyRunData_ok"
 
 # Check if the flag file exists and echo "already done" if it does
@@ -85,23 +92,29 @@ mkdir -p "${outfolder}/${inputs}"
 echo -e "\n# Copying RUN data locally"
 
 # Check if the adapter folder exists
-if [ -d "${runfolder}/${adapterfolder}" ]; then
-    cp -r "${runfolder}/${adapterfolder}" "${outfolder}/${inputs}"
+if [ -d "${runfolder}/${hififolder}" ]; then
+    cp ${runfolder}/${hififolder}/*bc*.bam* ${outfolder}/${inputs}/
 else
-    echo "Adapter folder not found: ${runfolder}/${adapterfolder}"
+    echo "HiFi folder not found: ${runfolder}/${hififolder}"
     return 1 # Exit the function with an error status
 fi
 
 # Check if the sample sheet exists
-if [ -f "${runfolder}/${samplesheet}" ]; then
-    cp "${runfolder}/${samplesheet}" "${outfolder}/${inputs}/"
-else
-    echo "Sample sheet not found: ${runfolder}/${samplesheet}"
+for i in {1..4}; do
+  samplesheet_var="bcM000${i}_samplesheet"
+  samplesheet_file="${!samplesheet_var}"
+  
+  if [ -f "$runfolder/$samplesheet_file" ]; then
+    cp "${runfolder}/$samplesheet_file" "${outfolder}/${inputs}/"
+    echo "Copied $samplesheet_file to ${outfolder}/${inputs}/"
+  else
+    echo "File $samplesheet_file does not exist in $runfolder"
     return 1 # Exit the function with an error status
-fi
+  fi
+done
 
 # Write the flag file upon successful completion
-touch "$flag_file"
+touch "${flag_file}"
 }
 
 function SkeraSplit {
@@ -113,26 +126,39 @@ if [ -f "${flag_file}" ]; then
     return 0 # Exit the function successfully
 fi
 
-mkdir -p "${outfolder}/${skera_results}"
+mkdir -p "${outfolder}/${skera_results}/bc0"{1..4}
 
 echo -e "\n# Running Skera de-concatenation"
 
-cmd="$(which skera) split \
-  ${outfolder}/${inputs}/${adapterfolder}/${movie}.hifi_reads.${adapterfolder}.bam \
-  barcode_files/MAS-Seq_Adapter_v2/mas12_primers.fasta \
-  ${outfolder}/${skera_results}/${movie}.skera.bam \
-  --num-threads ${nthr_skera} \
-  --log-level ${log_skera} \
-  --log-file ${outfolder}/${skera_results}/skera_run-log.txt"
+# run for each bc bam file and samplesheet
+for i in {1..4}; do
+  bam_var="${movie}.hifi_reads.bcM000${i}.bam"
+  bam_file="${outfolder}/${inputs}/${bam_var}"
 
-echo "# ${cmd}"
-eval ${cmd}
+  if [[ -f "$bam_file" ]]; then
+    cmd="$(which skera) split \
+      ${bam_file} \
+      ${BASEDIR}/barcode_files/MAS-Seq_Adapter_v2/mas12_primers.fasta \
+      ${outfolder}/${skera_results}/bc0${i}/skera.bam \
+      --num-threads ${nthr_skera} \
+      --log-level ${log_skera} \
+      --log-file ${outfolder}/${skera_results}/skera_run_bc0${i}-log.txt"
+
+    echo "# ${cmd}"
+    eval ${cmd}
+
+  else
+    echo "One or both files are missing"
+    return 1 # Exit the function with an error status
+  fi
+  
+done
 
 # Write the flag file upon successful completion
-touch "$flag_file"
+touch "${flag_file}"
 }
 
-function Lima {
+function Lima() {
 local flag_file="${outfolder}/${lima_results}/Lima_ok"
 
 # Check if the flag file exists and echo "already done" if it does
@@ -141,31 +167,58 @@ if [ -f "${flag_file}" ]; then
     return 0 # Exit the function successfully
 fi
 
-mkdir -p "${outfolder}/${lima_results}"
+mkdir -p "${outfolder}/${lima_results}/bc0"{1..4}
 
 echo -e "\n# Running Lima demultiplexing"
 
-cmd="$(which lima) \
-  ${outfolder}/${skera_results}/${movie}.skera.bam \
-  barcode_files/Kinnex16S_384plex_primers/Kinnex16S_384plex_primers.fasta \
-  ${outfolder}/${lima_results}/HiFi.bam \
-  --hifi-preset ASYMMETRIC \
-  --split-named \
-  --biosample-csv ${outfolder}/${inputs}/${samplesheet} \
-  --split-subdirs \
-  --num-threads ${nthr_lima} \
-  --log-level ${log_lima} \
-  --log-file ${outfolder}/${lima_results}/lima_run-log.txt"
+for i in {1..4}; do
+  bam_file="${outfolder}/${skera_results}/bc0${i}/skera.bam"
+  samplesheet_var="bcM000${i}_samplesheet"
+  samplesheet_file="${outfolder}/${inputs}/${!samplesheet_var}"
+  
+  if [[ -f "${samplesheet_file}" && -f "${bam_file}" ]]; then
 
-echo "# ${cmd}"
-eval ${cmd}
+    cmd="$(which lima) \
+      ${bam_file} \
+      ${BASEDIR}/barcode_files/Kinnex16S_384plex_primers/Kinnex16S_384plex_primers.fasta \
+      ${outfolder}/${lima_results}/bc0${i}/HiFi.bam \
+      --hifi-preset ASYMMETRIC \
+      --split-named \
+      --split-subdirs \
+      --biosample-csv ${samplesheet_file} \
+      --num-threads ${nthr_lima} \
+      --log-level ${log_lima} \
+      --log-file ${outfolder}/${lima_results}/lima_run_bc0${i}-log.txt"
+
+    echo "# ${cmd}"
+    eval ${cmd}
+
+    echo -e "# Creating barcode QC report"
+    projectnum=$(basename ${samplesheet_file} | cut -d "_" -f 1 | tr -d "\n")
+    cmd="${BASEDIR}/scripts/barcode_QC_Kinnex.sh \
+      -i ${outfolder}/${lima_results}/bc0${i}/HiFi.lima.counts \
+      -r ${BASEDIR}/scripts/barcode_QC_Kinnex.Rmd \
+      -m ${mincnt} \
+      -f ${qc_format} \
+      -p ${projectnum} \
+      -s ${samplesheet_file}"
+    
+    echo "# ${cmd}"
+    eval ${cmd} && mv barcode_QC_Kinnex.* ${outfolder}/${lima_results}/bc0${i}/
+
+  else
+    echo "One or both files are missing"
+    return 1 # Exit the function with an error status
+  fi
+
+done
 
 # Write the flag file upon successful completion
-touch "$flag_file"
+touch "${flag_file}"
 }
 
-function bam2fastq {
-local flag_file="${outfolder}/${lima_results}/bam2fastq_ok"
+function bam2fastq() {
+local flag_file="${outfolder}/${fastq_results}/bam2fastq_ok"
 
 # Check if the flag file exists and echo "already done" if it does
 if [ -f "${flag_file}" ]; then
@@ -173,38 +226,145 @@ if [ -f "${flag_file}" ]; then
     return 0 # Exit the function successfully
 fi
 
-mkdir -p "${outfolder}/${fastq_results}"
-cat /dev/null > ${outfolder}/job.list
+mkdir -p "${outfolder}/${fastq_results}/bc0"{1..4}
 
-echo -e "\n# Preparing job list from all lima BAM files"
+for i in {1..4}; do
+  lima_folder="${outfolder}/${lima_results}/bc0${i}"
+  samplesheet_var="bcM000${i}_samplesheet"
+  samplesheet_file="${outfolder}/${inputs}/${!samplesheet_var}"
+  joblist="${outfolder}/${fastq_results}/bc0${i}/job.list"
+  
+  if [[ -f "${samplesheet_file}" && -d "${lima_folder}" ]]; then
 
-for bam in $(find ${outfolder}/${lima_results} -name "*.bam"); do
-# rename sample from samplesheet 'Bio Sample'
-pfx=$(basename ${bam%.bam})
-bcpair=${pfx#HiFi.}
-biosample=$(grep ${bcpair} ${outfolder}/${inputs}/${samplesheet} | \
-  dos2unix | cut -d, -f 2 | tr -d "\n")
+    # initialize
+    cat /dev/null > ${joblist}
+    
+    echo -e "\n# Preparing job list from all bc0${i} lima BAM files"
+    
+    for bam in $(find ${lima_folder} -name "*.bam"); do
+    # rename sample from samplesheet 'Bio Sample'
+    pfx=$(basename ${bam%.bam})
+    bcpair=${pfx#HiFi.}
+    biosample=$(grep ${bcpair} ${samplesheet_file} | \
+      dos2unix | cut -d, -f 2 | tr -d "\n" | tr ' ' '_')
 
-echo "$(which bam2fastq) \
-    ${bam} \
-    --output ${outfolder}/${fastq_results}/${biosample} \
-    --num-threads ${nthr_bam2fastq}" >> job.list
+    echo "$(which bam2fastq) \
+        ${bam} \
+        --output ${outfolder}/${fastq_results}/bc0${i}/${biosample} \
+        --num-threads ${nthr_bam2fastq}" >> ${joblist}
+    done
+
+    # execute job list in batches of \${nthr_bam2fastq_par}
+    cmd="parallel -j ${par_bam2fastq} --joblog my_job_log.log \
+      < ${joblist} && (rm ${joblist} my_job_log.log)"
+
+    echo -e "\n# Executing bc0${i} ${joblist} in parallel batches"
+
+    echo "# ${cmd}"
+    eval ${cmd}
+  else
+    echo "One or both inputs are missing"
+    return 1 # Exit the function with an error status
+  fi
 done
 
-# execute job list in batches of \${nthr_bam2fastq_par}
-cmd="parallel -j ${par_bam2fastq} --joblog my_job_log.log \
-  < job.list && (rm job.list my_job_log.log)"
-
-echo -e "\n# Executing job list in parallel batches"
-
-echo "# ${cmd}"
-eval ${cmd}
-
 # Write the flag file upon successful completion
-touch "$flag_file"
+touch "${flag_file}"
 }
 
-function BundleResults {
+
+
+##################################
+##########  MAIN SCRIPT ##########
+##################################
+
+# Process command-line arguments
+while getopts "c:" opt; do
+  case "$opt" in
+    c)
+      CONFIG_FILE="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+# Check if the config file argument was provided
+if [ -z "$CONFIG_FILE" ]; then
+  echo "Error: Configuration file is required. Please use the -c option."
+  usage
+  exit 1
+fi
+
+# Check if the specified config file exists
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Error: Configuration file '$CONFIG_FILE' not found."
+  exit 1
+fi
+
+# Activate conda environment
+source /etc/profile.d/conda.sh
+conda activate "${myenv}" || {
+  echo "# the conda environment ${myenv} was not found on this machine"
+  echo "# please read the top part of the script!"
+  exit 1
+}
+
+# Check if required executables are present
+declare -a arr=( "yq" "skera" "lima" "bam2fastq" "pigz" "scripts/barcode_QC_Kinnex.sh" )
+for prog in "${arr[@]}"; do
+  hash "$prog" 2>/dev/null || { echo "# required ${prog} not found in PATH"; exit 1; }
+done
+
+# Read config in and prepare data and folders
+eval "$(parse_yaml "$CONFIG_FILE")"
+
+# create output folder
+mkdir -p "${outfolder}"
+
+# redirect all outputs to a log file
+cat /dev/null > "${outfolder}/runlog.txt"
+exec &> >(tee -a "${outfolder}/runlog.txt")
+
+# Print configuration
+PrintConfig
+
+# Run the pipeline steps
+time CopyRunData
+
+time SkeraSplit
+
+time Lima
+
+time bam2fastq
+
+exit 0
+
+
+
+
+
+
+
+
+
+#########################
+####### leftovers #######
+#########################
+
+#time BundleResults
+
+#time createArchive
+
+function BundleResults() {
 local flag_file="${outfolder}/BundleResults_ok"
 
 # Check if the flag file exists and echo "already done" if it does
@@ -232,7 +392,7 @@ mv ${outfolder}/${fastq_results} ${outfolder}/${final_results}/
 
 echo -e "# Creating barcode QC report"
 projectnum=$(echo ${samplesheet} | cut -d "_" -f 1 | tr -d "\n")
-cmd="scripts/barcode_QC_Kinnex.sh \
+cmd="${BASEDIR}/scripts/barcode_QC_Kinnex.sh \
   -i ${outfolder}/${final_results}/HiFi.lima.counts \
   -r scripts/barcode_QC_Kinnex.Rmd \
   -m ${mincnt} \
@@ -247,7 +407,7 @@ eval ${cmd} && mv barcode_QC_Kinnex.${qc_format} ${outfolder}/${final_results}/
 touch "$flag_file"
 }
 
-function createArchive {
+function createArchive() {
 local flag_file="Archive_ok"
 
 # Check if the flag file exists and echo "already done" if it does
@@ -283,18 +443,3 @@ else
     echo "Flag file not created. Verification failed."
 fi
 }
-
-
-############### PIPELINE ###############
-
-time CopyRunData
-
-time SkeraSplit
-
-time Lima
-
-time bam2fastq
-
-time BundleResults
-
-time createArchive
--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-TBD
+[(Nucleomics-VIB)](https://github.com/Nucleomics-VIB)
+
+Kinnex 16S deconcatenation and demultiplexing 
+==========
+
+## Revio run folder structure
+
+A typical run folder contains the following subfolders:
+
+* fail_reads
+* hifi_reads
+* metadata
+* pb_formats
+* statistics
+
+The pb_formats folder contains the files required for importing the run in SMRTLink.
+The hifi_reads folder contains bam files (one per kinnex barcode) required for the code below.
+
+## Method
+
+Each HiFi bam file is first deconcatenated into single Kinnex units using Skera (https://skera.how/). This process uses the list of 12 barcode pairs (6 for cDNA or 16 for single-cell)  corresponding to the assembled subunits.
+Deconcatenated reads are demultiplexed using the Lima package (https://lima.how/) and the sample-sheet provided by the lab to produce separate bam files for each barcoded 16S sample (max 384 per barcode).
+The resulting BAM files are converted to fastq using bam2fastq.
+Lima counts are used to produce a QC mozaic plot, provided to the customer when the project corresponds to the full barcode subset.
+
+## Command
+
+A bash workflow was prepared to streamline the different steps explained above. It relies on a config file that provides all specific arguments and naming necessary during the process.
+
+An example yaml file is shown next:
+
+```
+runfolder: "/data/pacbio_data/250204.Revio1.FCC"
+movie: "m84247_250204_101305_s4"
+hififolder: "hifi_reads"
+bcM0001:
+  bam: "m84247_250204_101305_s4.hifi_reads.bcM0001.bam"
+  samplesheet: "Exp4886_4898_4906_bc01_SMRTLink_Barcodefile_LDS20250128_edits.csv"
+bcM0002:
+  bam: "m84247_250204_101305_s4.hifi_reads.bcM0002.bam"
+  samplesheet: "Exp4921_1_bc02_SMRTLink_Barcodefile_LDS20250128.csv"
+bcM0003:
+  bam: "m84247_250204_101305_s4.hifi_reads.bcM0003.bam"
+  samplesheet: "Exp4921_2_bc03_SMRTLink_Barcodefile_LDS20250128.csv"
+bcM0004:
+  bam: "m84247_250204_101305_s4.hifi_reads.bcM0004.bam"
+  samplesheet:  "Exp4921_3_bc04_SMRTLink_Barcodefile_LDS20250128.csv"
+outfolder: "/data/pacbio_data/250204.Revio1.FCC/demux_analysis"
+qc_format: "html"
+inputs: "inputs"
+skera_results: "skera_results"
+lima_results: "lima_results"
+fastq_results: "fastq_results"
+log_skera: "INFO"
+log_lima: "INFO"
+nthr_skera: 48
+nthr_lima: 48
+par_bam2fastq: 12
+nthr_bam2fastq: 4
+mincnt: 12000
+final_results: "final_results"
+```
+
+The workflow command **Kinnex_16S_decat_demux.sh -c config.yaml** is run from /opt/biotools/Kinnex_16S_decat_demux_bash where the code is stored.
+
+Results are stored in the following folder structure:
+
+* inputs
+* skera_results
+* lima_results
+* fastq_results
+* runlog.txt
+
+the fastq_results folder contains the final fastq files, ready for archiving and tansfer when the full barcode set corresponds to a single project. In case several projects are mixed within a single barcode sublibrary, additinal work is required to isolate each project data based on sample-sheet information (not automatized in this code version)
+
+## Conclusion
+
+This aggregated analysis replaces the several-step SMRT-Link process which required presence of the operator at several points in time and was not simple to run. Resource optimisation for each subtask allows rapid analysis of a typical run and delivers data ready for delivery.
+
+<hr>
+
+<h4>Please send comments and feedback to <a href="mailto:nucleomics.bioinformatics@vib.be">nucleomics.bioinformatics@vib.be</a></h4>
+
+<hr>
+
+![Creative Commons License](http://i.creativecommons.org/l/by-sa/3.0/88x31.png?raw=true)
+
+This work is licensed under a [Creative Commons Attribution-ShareAlike 3.0 Unported License](http://creativecommons.org/licenses/by-sa/3.0/).
